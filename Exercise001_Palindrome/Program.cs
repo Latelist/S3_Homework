c96affbefe12ac0dbfd87b32b6c7e87d23d6127d
Salatchen <pozhivi.s.moe@gmail.com>
2022-08-28 23:50:07 +0300

    initial commit

---
+++ Exercise001_Palindrome/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+/* Сделала универсальную, чтобы обрабатывала любое число */
+
+
+
+string MaybePalindrome(string number)
+{
+    string palindrome = String.Empty;
+    int i = 0;
+    while (i <= number.Length - i)
+    {
+        if (number[i] == number[number.Length - i - 1])
+        {
+            if (i >= number.Length - i - 1)
+            {
+                palindrome = $"{number} is a palindrome";
+            }
+            ++i;
+        }
+        else
+        {
+            palindrome = $"{number} is not a palindrome";
+            break;
+        }
+    }
+    return palindrome;
+}
+
+Console.WriteLine("Enter number: ");
+string num = Console.ReadLine();
+while (num == String.Empty)
+{
+    Console.WriteLine("Please, enter number: ");
+    num = Console.ReadLine();
+}
+
+Console.WriteLine(MaybePalindrome(num));
---
+++ Exercise002_3DDistance/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит 
+расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+
+
+void FillMassive(int[] coordinates)
+{
+    for (int i = 0; i < coordinates.Length; i++)
+    {
+        Console.Write($"Enter coordinate {i + 1} of the point: ");
+        coordinates[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Point coordinates: ");
+    for (int i = 0; i < coordinates.Length; i++)
+    {   
+        if (i == coordinates.Length - 1) Console.WriteLine(coordinates[i]);
+        else Console.Write(coordinates[i] + ", ");
+    }
+    Console.WriteLine();
+}
+
+int[] point1 = new int[3];
+FillMassive(point1);
+
+int[] point2 = new int[3];
+FillMassive(point2);
+
+
+double FindDistance(int[] firstPoint, int[] secondPoint)
+{
+    double distance = Math.Sqrt(Math.Pow((firstPoint[0] - secondPoint[0]), 2)
+                        + Math.Pow((firstPoint[1] - secondPoint[1]), 2)
+                        + Math.Pow((firstPoint[2] - secondPoint[2]), 2));
+    return distance;
+}
+
+Console.WriteLine(FindDistance(point1, point2));
